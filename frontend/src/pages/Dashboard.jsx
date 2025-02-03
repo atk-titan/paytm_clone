76c9b3bfdafcf9b96f5222e4d21c4d6c613c21a3
atk-titan <gauravmane8451@gmail.com>
2025-02-04 01:27:40 +0530

    frontend start

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Dashboard = () => {
+  const Navigate = useNavigate();
+
+  return (
+    <div className=''>Dashboard</div>
+  );
+}
+
+export default Dashboard;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // ✅ Adjust paths based on your project structure
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };  
