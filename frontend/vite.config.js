4f7d6294c1a1181d10abbc31194a93818f9da120
atk-titan <gauravmane8451@gmail.com>
2025-02-06 03:25:15 +0530

    Paytm complete w deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base:"/simple_paytm/"
+  base:"/simple_paytm/frontend"
 })
