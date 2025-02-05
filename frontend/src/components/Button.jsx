63d2eef9f2351a656d2b10ff8a47ee38f622122d
atk-titan <gauravmane8451@gmail.com>
2025-02-05 17:31:07 +0530

    sign up done

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = (props) => {
     return (
-      <button className="w-full p-2 bg-black text-white rounded hover:bg-slate-500 transition-colors">
+      <button className="w-full p-2 bg-black text-white rounded hover:bg-slate-500 transition-colors" onClick={props.onClick}>
         {props.label}
       </button>
     );
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Heading from '../components/Heading';
 import Subheading from '../components/Subheading';
 import InputBox from '../components/InputBox';
 import Button from '../components/Button';
 import BottomWarning from '../components/BottomWarning';
+import axios from 'axios';
 
 const SignUp = () => {
+
+    const [firstName,setFirstName] = useState('');
+    const [lastName,setLastName] = useState('');
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+
+
   return (
     <div className='h-screen bg-slate-300 flex items-center justify-center lg:grid lg:grid-cols-3 place-items-center'>
      
         <div className=' lg:col-end-3 lg:col-span-1 px-10 py-4 flex items-center flex-col bg-white rounded-2xl text-center'>
           <Heading title="Sign Up" />
           <Subheading label="Enter your information to create your Account"/>
-          <InputBox label='First Name' placeholder='john' type='text'/>
-          <InputBox label='Last Name' placeholder='doe' type='text'/>
-          <InputBox label='email' placeholder='jdoe@email.com' type='email'/>
-          <InputBox label='Password' placeholder='password' type='password'/>
+          <InputBox label='First Name' placeholder='john' type='text' onChange={(e)=>{
+            setFirstName(e.target.value)
+          }}/>
+          <InputBox label='Last Name' placeholder='doe' type='text' onChange={(e)=>{
+            setLastName(e.target.value)
+          }}/>
+          <InputBox label='email' placeholder='jdoe@email.com' type='email' onChange={(e)=>{
+            setEmail(e.target.value)
+          }}/>
+          <InputBox label='Password' placeholder='password' type='password' onChange={(e)=>{
+            setPassword(e.target.value)
+          }}/>
           <div className='w-full mt-8'>
-            <Button label="Sign Up"/>
+            <Button label="Sign Up" onClick={async ()=>{
+              const response = await axios.post('http://localhost:3000/api/v1/user/signup',{
+                UserName: email,
+                FirstName:firstName,
+                LastName:lastName,
+                password:password
+              });
+              localStorage.setItem("token",response.data.token);
+            }}/>
           </div>
           <BottomWarning label='Already have an Account' button='Sign In' to='/signin'/>
         </div>
